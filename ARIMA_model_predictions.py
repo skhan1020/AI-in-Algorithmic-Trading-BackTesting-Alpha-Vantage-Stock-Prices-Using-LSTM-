47460b57f9bf3b5736ef0909596e176e3a0b7a4f
skhan <souratosh.khan86@gmail.com>
2019-10-18 10:25:14 -0700

    ARIMA model code : free of bugs -- accurate predictionns

---
+++ ARIMA_model_predictions.py
@@ -0,0 +1,102 @@
+import urllib.request, urllib.parse, urllib.error
+import config
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima_model import ARIMA
+from pandas.plotting import lag_plot
+from sklearn.metrics import mean_squared_error
+
+# Enter the Company Symbol
+
+
+SYMBOL = input('Enter Company Symbol: ')
+
+# Enter url Alpha Vantage API
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + SYMBOL + '&outputsize=full&apikey='+ config.public_api_key
+
+# Extract JSON data
+
+data = urllib.request.urlopen(url).read().decode()
+js = json.loads(data)
+
+
+close_price = list()
+dates = list()
+
+for item in js["Time Series (Daily)"]:
+    dates.append(item)
+    close_price.append(float(js['Time Series (Daily)'][item]['4. close']))
+
+# DataFrame of Close Prices
+
+stock_df = pd.DataFrame(data=close_price, index=dates)
+stock_df.index = pd.to_datetime(stock_df.index)
+stock_df = stock_df.sort_index(ascending=True)
+stock_df.columns = [SYMBOL]
+
+# Shape of DataFrame
+
+n = stock_df.shape[0]
+p = stock_df.shape[1]
+
+
+# Setting Training and Testing Data -- 80% of Data (Train)
+
+train_start = 0
+train_end = int(np.floor(0.8*n))
+test_start = train_end
+test_end = n
+
+
+# Autocorrelation Plots
+
+fig, axes = plt.subplots(2,2, figsize=(6, 8,))
+plt.title(SYMBOL + ' Autocorrelation plot ')
+
+ax_idcs = [(0,0), (0, 1), (1, 0), (1, 1)]
+
+for lag, ax_coords in enumerate(ax_idcs, 1):
+    ax_row, ax_col = ax_coords
+    axis = axes[ax_row][ax_col]
+    lag_plot(stock_df, lag=lag, ax=axis)
+    axis.set_title('Lag = ' + str(lag))
+
+plt.subplots_adjust(hspace=0.5)
+plt.show()
+
+# Forecasting using ARIMA Model
+
+train_ar = stock_df[:train_end].values
+test_ar = stock_df[train_end:].values
+
+history = [x for x in train_ar]
+predictions = list()
+for t in range(len(test_ar)):
+    model = ARIMA(history, order=(2,1,0))
+    model_fit = model.fit(disp=0)
+    output=model_fit.forecast()
+    yhat=output[0]
+    predictions.append(yhat)
+    obs=test_ar[t]
+    history.append(obs)
+
+error = mean_squared_error(test_ar, predictions)
+print('Error in ARIMA model', error)
+
+arima_predict = pd.DataFrame(predictions, index = stock_df[train_end:test_end].index, columns = [SYMBOL])
+
+# Plot ARIMA predictions
+
+plt.figure()
+plt.plot(stock_df[:train_end], color = 'b', label = 'Training Data')
+plt.plot(stock_df[train_end:], color = 'g', label = 'Test Data')
+plt.plot(arima_predict, color='orange', label = 'predicted data using ARIMA')
+plt.legend()
+plt.ylabel('Price in $')
+plt.title('ARIMA model predictions for '+SYMBOL)
+plt.show()
+
+
