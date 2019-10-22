f945395e7c3499514e291589697f6125fdddb597
skhan <souratosh.khan86@gmail.com>
2019-10-22 12:27:25 -0700

    added ACF and PACF plots

--- ARIMA_model_predictions.py
+++ ARIMA_model_predictions.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from pandas.plotting import lag_plot
 from sklearn.metrics import mean_squared_error
 
@@ -51,9 +52,29 @@ test_start = train_end
 test_end = n
 
 
-# Autocorrelation Plots
 
-fig, axes = plt.subplots(2,2, figsize=(6, 8,))
+# Autocorrelation vs Lag  --- ACF tails off -> AR model ; ACF cuts off after
+# lag p -> MA model
+
+plot_acf(stock_df, lags=5000)
+plt.xlabel('Lags')
+plt.ylabel('Autocorrelation (ACF) plot for ' + SYMBOL + ' stock prices')
+plt.title(SYMBOL + ' - ACF vs Lags')
+plt.show()
+
+# Partial Autocorrealtion vs Lag -- PACF cuts off after lag q -> AR model; PACF
+# tails off -> MA model
+
+plot_pacf(stock_df, lags=50)
+plt.xlabel('Lags')
+plt.ylabel('Partial Autocorrelation (PACF) plot for ' + SYMBOL + ' stock prices')
+plt.title(SYMBOL + ' - PACF vs Lags')
+plt.show()
+
+# Choosing AR/MA models for forecasting -- Autocorrelation plots for different
+# lags show a linear relationship -- autoregressive models
+
+fig, axes = plt.subplots(2,2, figsize=(6, 8))
 plt.title(SYMBOL + ' Autocorrelation plot ')
 
 ax_idcs = [(0,0), (0, 1), (1, 0), (1, 1)]
@@ -67,7 +88,15 @@ for lag, ax_coords in enumerate(ax_idcs, 1):
 plt.subplots_adjust(hspace=0.5)
 plt.show()
 
-# Forecasting using ARIMA Model
+# AutoCorrelation after 1st Order Differencing
+
+plot_acf(stock_df.diff().dropna(), lags=5000)
+plt.xlabel('Lags')
+plt.ylabel('ACF after 1st order Differencing')
+plt.title(SYMBOL + ' - ACF (d=1) vs Lags')
+plt.show()
+
+# Forecasting using ARIMA Model -- AutoRegressive Model with 1st Order Differencing 
 
 train_ar = stock_df[:train_end].values
 test_ar = stock_df[train_end:].values
@@ -91,7 +120,7 @@ arima_predict = pd.DataFrame(predictions, index = stock_df[train_end:test_end].i
 # Plot ARIMA predictions
 
 plt.figure()
-plt.plot(stock_df[:train_end], color = 'b', label = 'Training Data')
+# plt.plot(stock_df[:train_end], color = 'b', label = 'Training Data')
 plt.plot(stock_df[train_end:], color = 'g', label = 'Test Data')
 plt.plot(arima_predict, color='orange', label = 'predicted data using ARIMA')
 plt.legend()
