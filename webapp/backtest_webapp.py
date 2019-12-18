3cd7f3ef5f2ccbc0ad724b036a19c4c8efd0dbc8
skhan <souratosh.khan86@gmail.com>
2019-12-17 19:10:55 -0700

    added new functionalities; equity curve generation

--- webapp/backtest_webapp.py
+++ webapp/backtest_webapp.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import io
 import base64
 import sys
+from tradingstrategy_webapp import MovingAverageCrossStrategy as cs
+from tradingstrategy_webapp import MarketOnClosePortfolio as pf
 sys.path.insert(1, '../')
 from models import arima_model as am
 from models import lstm_model as lm
@@ -56,7 +58,8 @@ def arima_plots(data, symbol, train_len, test_len):
     return create_io_obj()
 
 
-def lstm_plots(data, symbol, train_start, train_end, test_start, test_end):
+def lstm_plots(data, symbol, train_start, train_end, test_start, test_end, short_window, long_window, initial_capital):
+# def lstm_plots(data, symbol, train_start, train_end, test_start, test_end):
 
     #####   Implementing the LSTM Model to Predict Future Prices   #####
 
@@ -87,5 +90,44 @@ def lstm_plots(data, symbol, train_start, train_end, test_start, test_end):
 
     bytes_image2 = create_io_obj()
 
-    return bytes_image1, bytes_image2
+    mac = cs(symbol, lstm_predictions, short_window, long_window)
+    signals = mac.generate_signals()
+    
+    # Plot of Test Prices and Predicted Prices along with Markers for Trading
+    # Strategy
+
+    fig = plt.figure(figsize=(8,6))
+    ax1 = fig.add_subplot(111, ylabel='Price in $')
+    dev_set[symbol].plot(ax=ax1, color = 'k', lw=2, label='actual data')
+    lstm_predictions[symbol].plot(ax=ax1, color = 'm', lw=2, label='predicted data')
+    signals[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2)
+    plt.legend()
+
+    #buy signal
+    ax1.plot(signals.loc[signals.positions == 1.0].index, signals.short_mavg[signals.positions == 1.0], '^', markersize=10, color= 'g')
+
+    #sell signal
+    ax1.plot(signals.loc[signals.positions == -1.0].index, signals.short_mavg[signals.positions == -1.0], 'v', markersize=10, color = 'r')
+    plt.title('Trading strategy on predicted data with green (buy) and red (sell) markers')
+
+    bytes_image3 = create_io_obj()
+    
+    pft = pf(symbol, lstm_predictions, signals, initial_capital)
+    portfolio = pft.backtest_portfolio()
+
+
+    fig = plt.figure(figsize=(8,6))
+
+    # Plot equity curve in dollars
+
+    ax1 = fig.add_subplot(111, ylabel='Portfolio Value in $')
+    portfolio['total'].plot(ax=ax1, lw=2, label = 'Total Cash = Cash + Holdings')
+    ax1.plot(portfolio.loc[signals.positions == 1.0].index, portfolio.total[signals.positions == 1.0], '^', markersize=10, color='g')
+    ax1.plot(portfolio.loc[signals.positions == -1.0].index, portfolio.total[signals.positions == -1.0], 'v', markersize=10, color='r')
+    plt.title('Plot of equity curve with buy (green) and sell (red) markers')
+
+    bytes_image4 = create_io_obj()
+
+    return bytes_image1, bytes_image2, bytes_image3, bytes_image4
+    # return bytes_image1, bytes_image2
 
