ce25332d17bc58c733e5f87bd17314e593246391
skhan <souratosh.khan86@gmail.com>
2019-12-17 19:11:37 -0700

    added new functionalities; new graphs; home and results page

--- webapp/flask_app.py
+++ webapp/flask_app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import numpy as np
 import backtest_webapp
 import sys
-sys.path.insert(1, '../')
-import stock_data
+import stock_data_webapp
 
 app = Flask(__name__)
- 
-@app.route('/graphs')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    return render_template("main.html")
+
+@app.route('/graphs', methods=['GET', 'POST'])
 def graphs():
 
-    alpha_API = stock_data
-    stock_df, SYMBOL = alpha_API.get_data()
+
+    args = request.form.to_dict('company')
+    print(args)
+
+    alpha_API = stock_data_webapp
+    stock_df, SYMBOL = alpha_API.get_data(args['company'])
+
+    short_window = int(args['short_window'])
+    long_window = int(args['long_window'])
+    initial_capital = float(args['initial_capital'])
+
 
     app_obj = backtest_webapp
 
@@ -26,13 +38,15 @@ def graphs():
 
     graph1_url = app_obj.plot_stock_prices(stock_df, SYMBOL)
     graph2_url = app_obj.arima_plots(stock_df, SYMBOL, train_end, test_end)
-    graph3_url, graph4_url = app_obj.lstm_plots(stock_df, SYMBOL, train_start, train_end, test_start, test_end)
+    graph3_url, graph4_url, graph5_url, graph6_url = app_obj.lstm_plots(stock_df, SYMBOL, train_start, train_end, test_start, test_end, short_window, long_window, initial_capital)
  
     return render_template('graphs.html',
     graph1=graph1_url,
     graph2=graph2_url,
     graph3=graph3_url,
-    graph4=graph4_url)
+    graph4=graph4_url,
+    graph5=graph5_url,
+    graph6=graph6_url)
  
 if __name__ == '__main__':
     app.debug = True
